12d462cf2912d1a8525c4128a23448a7ea7a939c
Jeremy100 <pynejeremy@gmail.com>
2026-03-05 10:08:55 +0000

    Finished Exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
-using System;
+ using System;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        
+        Console.WriteLine("What is your first Name? ");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last Name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}!");
     }
 }
